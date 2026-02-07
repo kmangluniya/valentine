258fd8fbaaa2a8e1c7f56c851a343e1f2eb1eb48
kmangluniya <kalpitmangluniya@gmail.com>
2026-02-07 21:56:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -94,5 +94,15 @@ export default function Page() {
 
 const Footer = () => {
   return (
+        <a
+      className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
+      href=""
+      target="__blank"
+    >
+      Made with{" "}
+      <span role="img" aria-label="heart">
+        ❤️
+      </span>
+    </a>
   );
 };
